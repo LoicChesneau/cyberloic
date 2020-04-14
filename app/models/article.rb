8ef0d4e874b993a2d162ede367781721e2604645
Loic Chesneau <loic.chesneau@lrqdo.fr>
2020-04-14 19:30:46 +0200

    more tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,16 @@
 class Article < ApplicationRecord
+  validates :name
+  def index
+    @articles = Article.all
+  end
+
+  def create
+    @article = Article.create()
+    # redirect_to(new_cocktail_dose_path(@cocktail.id))
+    #   redirect_to cocktail_path(@cocktail)
+    # else
+    #   render :new
+    # end
+  end
+
 end
