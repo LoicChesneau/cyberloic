a709cb68195468fc5bcf6bc5e64a75ddfe084728
Loic Chesneau <loic.chesneau@lrqdo.fr>
2020-04-14 20:18:05 +0200

    stuff

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,16 +1,2 @@
 class Article < ApplicationRecord
-  validates :name
-  def index
-    @articles = Article.all
-  end
-
-  def create
-    @article = Article.create()
-    # redirect_to(new_cocktail_dose_path(@cocktail.id))
-    #   redirect_to cocktail_path(@cocktail)
-    # else
-    #   render :new
-    # end
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#articles'
-  get 'articles', to: 'pages#articles'
 
   # Generic syntax:
   # verb 'path', to: 'controller#action'
